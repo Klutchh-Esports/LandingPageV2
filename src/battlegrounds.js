30838a1f0618b7ba7b3f95e2bf9618b3f1e00e93
Prakhar <prakhar@klutchh.in>
2022-08-07 22:04:57 +0530

    Battlegrounds

--- src/battlegrounds.js
+++ src/battlegrounds.js
@@ -2,7 +2,16 @@ import {html, render} from '../getLitHTML.js';
         // Define a template
 const myTemplate = () => html
     `<div>
-        <h1>Battlegrounds</h1>
+        <h1><span class='text-klutchh-purple'>Battlegrounds</span> You Can Enter</h1>
+        <div class='row1 flex'>
+            <img width='250px' src='../images/valorant_card.avif' alt='valorant' />
+            <img width='250px' src='../images/valorant_card.avif' alt='valorant' />
+            <img width='250px' src='../images/valorant_card.avif' alt='valorant' />
+        </div>
+        <div class='row1 flex'>
+            <img width='250px' src='../images/valorant_card.avif' alt='valorant' />
+            <img width='250px' src='../images/valorant_card.avif' alt='valorant' />
+        </div>
     </div> `;
         // Render the template to the document
 render(myTemplate(), document.querySelector('#battlegrounds'));
